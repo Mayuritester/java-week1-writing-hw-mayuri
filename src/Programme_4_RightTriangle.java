75062dccab228fd3fba0d186b9e3c517ff102c59
Mayuritester <computer.tester009@gmail.com>
2024-10-16 19:16:37 +0100

    java-week1-writing-hw-mayuri

---
+++ src/Programme_4_RightTriangle.java
@@ -0,0 +1,16 @@
+public class Programme_4_RightTriangle {
+/**
+ * Write 4 triangle
+ */
+public static void main(String[] args) {
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+    System.out.println("*****");
+    System.out.println("******");
+
+}
+
+
+}
---
+++ src/programme_5_Triangle.java
@@ -0,0 +1,23 @@
+public class programme_5_Triangle {
+    /**
+     * 5. Write a Java Program to print below triangle
+     *            *
+     *           * *
+     *          * * *
+     *         * * * *
+     *        * * * * *
+     *       * * * * * *
+     *
+     */
+
+    public static void main(String[] args) {
+        System.out.println("       *       ");
+        System.out.println("      * *      ");
+        System.out.println("     * * *     ");
+        System.out.println("    * * * *    ");
+        System.out.println("   * * * * *   ");
+        System.out.println("  * * * * * *  ");
+
+
+    }
+}
